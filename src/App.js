b3ac9aeefdd56044d5c458919faae2aa94804c3c
Joiyad <20bee012@nith.ac.in>
2022-08-07 23:46:15 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css'
+
 
 function App() {
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const notifySuccess = () => toast("You have successfully registered");
+  const notifyFailure = () => toast("Kindly fill the form completely")
+
+  const registerUser = (e) => {
+    e.preventDefault();
+    if (userName === '' || email === '' || password === '') {
+      notifyFailure();
+    } else {
+      notifySuccess();
+      setUserName('');
+      setEmail('');
+      setPassword('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={registerUser}>
+        <ToastContainer/>
+        <h1>Registration Form</h1>
+        <input value={userName} onChange={(e) => setUserName(e.target.value)} type="text" placeholder="Name" /><br/>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" /><br/>
+        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" /><br/>
+        <button type="submit"><p>Submit</p></button>
+      </form>
     </div>
   );
 }
